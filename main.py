7983b4a135ad4523c53cfea2da04c5c42c00184f
Mahamudul-Hasan-Abir <mhabir10@gmail.com>
2026-09-16 11:56:14 +0600

    added get examples

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
 from fastapi import FastAPI
-
+from mockData import products
 app = FastAPI()
 
 
+@app.get("/")
+def home():
+    return {"message": "Welcome to the FastAPI server!"}
+
+
+
+@app.get("/products")
+
+def get_products():
+
+    return products
+
+
+
+
+# Path params
+@app.get("/product/{product_id}")
+
+def get_single_product(product_id:int):
+    # if product available with the id return product,else return error message
+  
+    for product in products:
+        if product.get("id") == product_id:
+            return product
+    return {"error": "Product not found!"}
+
+
+# query params
+
+@app.get("/greet")
+def greet_user(name:str,age:int ):
+    return {"message": f"Hello, {name}! You are {age}."}
---
+++ mockData.py
@@ -0,0 +1,62 @@
+products = [
+    {
+        "id": 1,
+        "title": "Mobile Phone",
+        "price": 1000,
+        "count": 10
+    },
+    {
+        "id": 2,
+        "title": "Laptop",
+        "price": 2500,
+        "count": 5
+    },
+    {
+        "id": 3,
+        "title": "Tablet",
+        "price": 800,
+        "count": 8
+    },
+    {
+        "id": 4,
+        "title": "Headphones",
+        "price": 150,
+        "count": 20
+    },
+    {
+        "id": 5,
+        "title": "Keyboard",
+        "price": 100,
+        "count": 15
+    },
+    {
+        "id": 6,
+        "title": "Mouse",
+        "price": 50,
+        "count": 25
+    },
+    {
+        "id": 7,
+        "title": "Monitor",
+        "price": 500,
+        "count": 7
+    },
+    {
+        "id": 8,
+        "title": "Smart Watch",
+        "price": 300,
+        "count": 12
+    },
+    {
+        "id": 9,
+        "title": "Bluetooth Speaker",
+        "price": 200,
+        "count": 18
+    },
+    {
+        "id": 10,
+        "title": "Power Bank",
+        "price": 80,
+        "count": 30
+    }
+]
